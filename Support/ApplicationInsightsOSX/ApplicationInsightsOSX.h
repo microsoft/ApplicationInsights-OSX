d46517d3059274716a085d053030dab2f09d844e
Andreas Linde <anlinde@microsoft.com>
2015-04-28 11:59:21 +0200

    Import the proper header for temp framework name

--- Support/ApplicationInsightsOSX/ApplicationInsightsOSX.h
+++ Support/ApplicationInsightsOSX/ApplicationInsightsOSX.h
@@ -16,4 +16,4 @@ FOUNDATION_EXPORT const unsigned char ApplicationInsights_OSXVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <ApplicationInsights_OSX/PublicHeader.h>
 
-
+#import "ApplicationInsights.h"
