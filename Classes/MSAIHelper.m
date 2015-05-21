46d535320cf25237bd776426d573d220d861662c
Lukas Spieß <lspiess@microsoft.com>
2015-05-21 23:43:41 +0200

    Add macro checks for APIs only available in 10.10 SDK

--- Classes/MSAIHelper.m
+++ Classes/MSAIHelper.m
@@ -152,11 +152,13 @@ NSString *msai_osVersionBuild(void) {
   free(result);
   
   NSString* osVersion = nil;
-
+  
+#if __MAC_OS_X_VERSION_MAX_ALLOWED > 1090
   if ([[NSProcessInfo processInfo] respondsToSelector:@selector(operatingSystemVersion)]) {
     NSOperatingSystemVersion osSystemVersion = [[NSProcessInfo processInfo] operatingSystemVersion];
     osVersion = [NSString stringWithFormat:@"%ld.%ld.%ld", (long)osSystemVersion.majorVersion, (long)osSystemVersion.minorVersion, (long)osSystemVersion.patchVersion];
   } else {
+#endif
     SInt32 major, minor, bugfix;
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wdeprecated"
@@ -166,7 +168,9 @@ NSString *msai_osVersionBuild(void) {
     if ((!err1) && (!err2) && (!err3)) {
       osVersion = [NSString stringWithFormat:@"%ld.%ld.%ld", (long)major, (long)minor, (long)bugfix];
     }
+#if __MAC_OS_X_VERSION_MAX_ALLOWED > 1090
   }
+#endif
   
   return [NSString stringWithFormat:@"%@ (%@)", osVersion, osBuild];
 }
