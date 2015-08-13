69ab63acf917652383082edca7612a288218c3fa
Andreas Linde <anlinde@microsoft.com>
2015-08-13 11:24:16 +0200

    Clean up small includes

--- Classes/ApplicationInsights.h
+++ Classes/ApplicationInsights.h
@@ -1,4 +1,4 @@
-#import <Cocoa/Cocoa.h>
+#import <Foundation/Foundation.h>
 
 //! Project version number for ApplicationInsights.
 FOUNDATION_EXPORT double ApplicationInsightsVersionNumber;
--- Classes/ApplicationInsightsPrivate.h
+++ Classes/ApplicationInsightsPrivate.h
@@ -1,4 +1,3 @@
-#import <Foundation/Foundation.h>
 #import "MSAIApplicationInsights.h"
 
 NS_ASSUME_NONNULL_BEGIN
